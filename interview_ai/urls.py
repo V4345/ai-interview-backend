241f6d55ad16e4b1af9ef9d6972d6633badd2670
Vishal Talwar <talwarv63@gmail.com>
2026-02-28 19:20:37 +0530

    Add working health check

--- interview_ai/urls.py
+++ interview_ai/urls.py
@@ -6,7 +6,7 @@ def health_check(request):
     return HttpResponse("OK", status=200)
 
 urlpatterns = [
-    path('', health_check),
+    path('', health_check),   # IMPORTANT
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
