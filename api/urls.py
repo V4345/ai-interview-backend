2a017ba4f5bf70fd8c9fa17d0e54b095c95ddb9e
Vishal Talwar <talwarv63@gmail.com>
2026-02-28 19:41:36 +0530

    Add JWT token route

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 from .views import register_user
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("start/", views.start_interview),
     path("submit/", views.submit_answer),
     path("history/", views.interview_history),
     path("dashboard/", views.dashboard_summary),
-    path("register/", register_user)
+    path("register/", register_user),
 
+    # 🔥 ADD THIS LINE
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
